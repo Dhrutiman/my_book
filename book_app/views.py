396722c1eb4fd4899dbe20de216326ded733017f
Dhrutiman Sethi <dhrutiman123@gmail.com>
2020-01-15 00:00:11 +0530

    Add frontend and other library

--- book_app/views.py
+++ book_app/views.py
@@ -1,6 +1,17 @@
+import requests
 from django.shortcuts import render
-from django.http import HttpResponse
+from bs4 import BeautifulSoup
 
 # Create your views here.
 def home(request):
     return(render(request, 'book_app\home.html'))
+
+def new_search(request):
+    search= request.POST.get('search')
+    search_type = request.POST.get('Search_type')
+    #print(search,search_type)
+    data_to_send={
+        'search': search,
+        'type':search_type,
+    }
+    return render(request,"book_app\\new_search.html",data_to_send)
