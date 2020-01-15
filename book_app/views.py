8768057e221febe2626364b45a29ab2d351167e8
Dhrutiman Sethi <dhrutiman123@gmail.com>
2020-01-15 16:50:09 +0530

    after adding beautiful_soup

--- book_app/views.py
+++ book_app/views.py
@@ -1,17 +1,69 @@
 import requests
+from requests.compat import quote_plus
 from django.shortcuts import render
+from . import models
 from bs4 import BeautifulSoup
 
 # Create your views here.
+main_url='http://gen.lib.rus.ec/search.php?req={}&open=0&res=50&view=simple&phrase=1&column={}'
 def home(request):
     return(render(request, 'book_app\home.html'))
+
 
 def new_search(request):
     search= request.POST.get('search')
     search_type = request.POST.get('Search_type')
-    #print(search,search_type)
+
+    models.Search.objects.create(search=search,type=search_type)
+
+    url_f=main_url.format(quote_plus(search),search_type)
+    response=requests.get(url_f)
+    page_html=response.text
+
+    page_data=BeautifulSoup(page_html,features='html.parser')
+
+    post_data=page_data.find_all('tr',{'valign':'top','bgcolor':('#C6DEFF','')})
+
+    for_front_data=[]
+    for post in post_data:
+
+        book=post.find_all('td')
+        book_id=book[0].text
+        book_data=book[2].find('a',{'id':book_id})
+        book_author=book[1].find_all('a')
+        book_year=book[4].text
+        book_lng=book[6].text
+        book_link=book[9].find('a').get('href')
+        book_formate=book[8].text
+
+        author=""
+        i=0
+        for name in book_author:
+            if author!="":
+                author+=", "
+            author=author+name.text
+            i+=1
+            if(i==3):
+                break
+
+        ext=book_data.find_all('i')
+        ex=""
+        p=0
+        for i in ext:
+            p=1
+            ex=ex+i.text
+
+        book_title=book_data.text
+        if p==1:
+            book_name=book_title[:len(book_title)-len(ex)-1]
+        else:
+            book_name=book_title
+
+        for_front_data.append((book_name,author,book_lng,book_year,book_link,book_formate))
+
     data_to_send={
         'search': search,
         'type':search_type,
+        'for_front_data':for_front_data,
     }
     return render(request,"book_app\\new_search.html",data_to_send)
